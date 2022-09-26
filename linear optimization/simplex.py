66e86be96126b6c3460cb8ab97a9227a2a7332a4
Lucas Donaire <lpdonaire@outlook.com>
2022-09-26 13:02:48 -0300

    linear regression

--- linear optimization/simplex.py
+++ linear optimization/simplex.py
@@ -23,13 +23,13 @@ class Solve:
             for j in range(len(self.A[0])):
                 if j in indices:
                     linha.append(self.A[0,j])
-            B = np.concatenate(M,linha,axis=0)
+            B = np.concatenate(B,linha,axis=0)
         return B
     def solveBase(self,B,indices):
         invB = np.linalg.inv(B)
         xb = iter(np.dot(invB,self.b))
         x = []
-        for i in range(m):
+        for i in range(self.m):
             if x in indices:
                 x.append(next(xb))
             else:
